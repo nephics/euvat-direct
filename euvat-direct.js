ccd0cbd46715458ef26c0d114b21ebffdf309760
Jacob Svensson <jacob@nephics.se>
2021-02-16 13:40:36 +0100

    update country codes
    list obtained from https://ec.europa.eu/taxation_customs/vies/faq.html#item_7

--- euvat-direct.js
+++ euvat-direct.js
@@ -72,7 +72,7 @@ MIT License
     });
   };
 
-  country_codes = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'GB', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
+  country_codes = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'XI'];
 
   showResult = function(html) {
     return $('#result').html(html);
